1b95b90a3ec2482ad37bc17de29718fcbf1bc649
gamingsaha <85555051+gamingsaha@users.noreply.github.com>
2023-08-15 20:05:12 +0530

    Update search.py

--- plugins/search.py
+++ plugins/search.py
@@ -31,7 +31,7 @@ async def search(bot, message):
           buttons = []
           for movie in movies: 
               buttons.append([InlineKeyboardButton(movie['title'], callback_data=f"recheck_{movie['id']}")])
-          msg = await message.reply_photo(photo="https://telegra.ph/file/cf6706158b0bfaf436f54.jpg",
+          msg = await message.reply_photo(photo="https://graph.org/file/ac1a6b9cff40723df83d1.jpg",
                                           caption="<b><I>I Couldn't find anything related to Your Query😕.\nDid you mean any of these?</I></b>", 
                                           reply_markup=InlineKeyboardMarkup(buttons))
        else:
