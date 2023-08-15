c8db27ea174a390f14dc0d25aefa1bd23a87f9db
gamingsaha <85555051+gamingsaha@users.noreply.github.com>
2023-08-15 19:19:16 +0530

    Update info.py

--- info.py
+++ info.py
@@ -3,6 +3,6 @@ API_HASH     = "bd1969abffc09157194e1c8d24fb97df"
 BOT_TOKEN    = "6559553176:AAHzzuDjBug664agRuZTdyVZ6z5THSx9Jm8" 
 SESSION      = "AQGqOOgALh8xOlbBc3mEg2T4Ot-qzoHMjkofJyGneZ9BLZFIeh73HVxWds8YTZDiShU0fSuvqE7nAWkd79UmhVddQ21T73w0az_t79CP3T5Hh8eb4RcUWLs_X4QjudWwEgixrPZ4_6zsCLeou_-IfQ7A8kkfbSvfzsrV9n8KSWyWvsq9c75PVHoixuRiwVnRUKuDMSBxXYUupoHPy7pvb3M_MdNto0Gd6HS1RFwShfNodzXjbNVAyDZ9F-aug64PIE7pVnVtIaw_jmqnalTsiuTCgvgUVHEL7p59kh1JixeYmCYOu5fmowGWJL77Lf0zfyTfXmaS5JjgrYsvFcvN9nHxaxdvNgAAAAGJZuu8AA" 
 DATABASE_URI = "mongodb+srv://cheapd148:cheapd148@cluster0.bxejgg4.mongodb.net/?retryWrites=true&w=majority" 
-LOG_CHANNEL  = -1001837593769 
+LOG_CHANNEL  = -1001976886026 
 ADMIN        = 6600190908 
-CHANNEL      = -1001986377785
+CHANNEL      = "@Sseries_Area"
