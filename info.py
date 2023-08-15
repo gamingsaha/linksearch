8e6fcca82d1c89f0dcf31c04f59387a38181cc57
gamingsaha <85555051+gamingsaha@users.noreply.github.com>
2023-08-15 18:26:22 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,6 +1,6 @@
 API_ID       = 27932904 
 API_HASH     = "bd1969abffc09157194e1c8d24fb97df" 
-BOT_TOKEN    = "6103629593:AAH2taLm7GTTD1BBQlUk18qVvMgNbjsNna8" 
+BOT_TOKEN    = "6559553176:AAHzzuDjBug664agRuZTdyVZ6z5THSx9Jm8" 
 SESSION      = "AQGqOOgAXgYCGWoaEes2cck5NJsI5lyO_GmsJG1nYL0qqoiGJTGaPi3K-3hDrnr1Fv_40qAaG01Ie8qPdE9wnOgU-BKC-Wri1_6iz2BH5Z0zCz6xbs4TfFzOW_eHKeHF9pMrqCRsbmaNbrgesAIbM4xp1r3dBaAcFwEoIaymyD2trlwN3KrIt5B2CkLRUlVGUfUiNv9PQvI6dcANX9ZMWTC8t-lFzsiG5k-MTBZbezbYGt7tOPUV-hjojnRg8iI6lVnP6p24ShG4wvaiNE7Zyb5bSW8cjso1-H_MwpSHhI-De3bHUsztonkZGfMpdCy0De3tqdKa33ab3_Ov2M7ZO9d3SVOEHwAAAAGJZuu8AA" 
 DATABASE_URI = "mongodb+srv://cheapd148:cheapd148@cluster0.bxejgg4.mongodb.net/?retryWrites=true&w=majority" 
 LOG_CHANNEL  = -1001837593769 
