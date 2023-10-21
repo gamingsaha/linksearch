9b1126313dae8dc56f74999fbf1a2b630fc484ac
gamingsaha <85555051+gamingsaha@users.noreply.github.com>
2023-10-21 21:42:33 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,7 +1,7 @@
 API_ID       = 27932904 
 API_HASH     = "bd1969abffc09157194e1c8d24fb97df" 
 BOT_TOKEN    = "6559553176:AAHzzuDjBug664agRuZTdyVZ6z5THSx9Jm8" 
-SESSION      = "AQGqOOgAWs6ePV7-qkwVFuUObnJ0l7CVjSVwyD2bxbqy1dqYWPRq_wv7waOOzQfkILcV_nSqDH8TZERsJzNTdnCCIP5L8EsZo3yf87ijOqQXWmqV_yTgPkumTlKs1Dp4GJ6Wi5lly18T6vdQP8LGXPqO6f29QdtdBag7yEqwKlqDd0Vnifw9YbpRdMqW0GyURFMPu3BC0lEurOI1vsbye7Z8p8zsDNENr635_zMWgWuwZmOdZbvM8M_y-pqPacibyU-Qek9kn-bIkde6Eq56F59bx8a-cvQABjHT6rHCb-rZ0mLQ3dvqmk39iJp_r4LRc2m8CM_jPTs0HjvCCZMJ8ckb0IgRUAAAAAGJZuu8AA" 
+SESSION      = "AQGqOOgAZIAmkNqGwQfeka9ThxKAh_F-pRtLysxoLNLWD-4dYkhxVf3RQWnZK2Cz37Zu9udK1Zp9DQpoZM13KydCdHmZrvcvwfi8a0qSv0M087vUJ8hLBHOz4gQfWhgTg5W935quhS6vv2Eie1a6fSynp8bZDvsEnWA5AIvh-Yh1V3ABvNmdXh6bU-qiZwA3KgWZl2m7Z72ss9n0WAOirfa1LGoA0oi-GAMshrK96hG2n8oJtPxrFjQvvcVRrg8c1GnS1USpUnTrltpkec-wB6N2MgaV3z7KuzvtQ2YO_bnrbdGQgGksjnKKxmWpppa_hwzWSxdOsmFa4Sq_OoQzkuuLno1ltQAAAAGJZuu8AA" 
 DATABASE_URI = "mongodb+srv://cheapd148:cheapd148@cluster0.bxejgg4.mongodb.net/?retryWrites=true&w=majority" 
 LOG_CHANNEL  = -1001976886026 
 ADMIN        = 6600190908 
