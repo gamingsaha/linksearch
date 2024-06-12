99dedd18fce2b87461610014a18d192aa546ffd1
gamingsaha <85555051+gamingsaha@users.noreply.github.com>
2024-06-12 11:59:00 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,14 @@
+![Typing SVG](https://readme-typing-svg.herokuapp.com/?lines=CHANNEL+POST+FILTER+BOT!;CREATED+BY+SSERIES+AREA!)
+</p>
+
+</p>
+<h1 align="center">
+  <b>Channel-Post-Filter</b>
+</h1>
+
+## TG Channel [@Sseries_Area](https://t.me/Sseries_Area)
+
+## Telegram 
+
+
+* [![Contact](https://img.shields.io/static/v1?label=Contact&message=On+Telegram&color=critical)](https://t.me/Sseries_Area_Help_Support)
---
+++ info.py
@@ -0,0 +1,8 @@
+API_ID       = 27932904 
+API_HASH     = "bd1969abffc09157194e1c8d24fb97df" 
+BOT_TOKEN    = "6559553176:AAHzzuDjBug664agRuZTdyVZ6z5THSx9Jm8" 
+SESSION      = "AQGqOOgAZIAmkNqGwQfeka9ThxKAh_F-pRtLysxoLNLWD-4dYkhxVf3RQWnZK2Cz37Zu9udK1Zp9DQpoZM13KydCdHmZrvcvwfi8a0qSv0M087vUJ8hLBHOz4gQfWhgTg5W935quhS6vv2Eie1a6fSynp8bZDvsEnWA5AIvh-Yh1V3ABvNmdXh6bU-qiZwA3KgWZl2m7Z72ss9n0WAOirfa1LGoA0oi-GAMshrK96hG2n8oJtPxrFjQvvcVRrg8c1GnS1USpUnTrltpkec-wB6N2MgaV3z7KuzvtQ2YO_bnrbdGQgGksjnKKxmWpppa_hwzWSxdOsmFa4Sq_OoQzkuuLno1ltQAAAAGJZuu8AA" 
+DATABASE_URI = "mongodb+srv://cheapd148:cheapd148@cluster0.bxejgg4.mongodb.net/?retryWrites=true&w=majority" 
+LOG_CHANNEL  = -1001976886026 
+ADMIN        = 6600190908 
+CHANNEL      = "@Sseries_Area"
